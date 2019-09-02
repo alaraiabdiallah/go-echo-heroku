252556c621a5cb352a5c387acea90237e4710302
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2019-09-03 00:51:09 +0700

    port

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/alaraiabdiallah/golang-heroku-starter/src/routes"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -15,5 +18,5 @@ func main() {
 	api := e.Group("/api")
 	routes.APIV1(api)
 
-	e.Logger.Fatal(e.Start(":5000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }

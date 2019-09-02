9d95151ac929557fa2ebdd3ee7298a99133593d9
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2019-09-03 00:43:31 +0700

    Route

--- src/routes/apiv1.go
+++ src/routes/apiv1.go
@@ -7,7 +7,7 @@ import (
 )
 
 func APIV1(api *echo.Group) {
-	v1 := api.Group("v1")
+	v1 := api.Group("/v1")
 	v1.GET("/", func(ctx echo.Context) error {
 		return ctx.String(http.StatusOK, "Hello World")
 	})
